a2aee5186e87b0d11e35fb31a014fc88a27ce0f5
sag <step.on.off@ya.ru>
2022-05-16 16:09:49 +0300

    build

--- src/js/core.js
+++ src/js/core.js
@@ -49,7 +49,7 @@ export default class BomTable {
 
         this.minColWidth = 60
         this.isTouch = this.config.touchSupport && 'ontouchstart' in window
-        this.version = '2.4.7'
+        this.version = '2.4.8'
 
         this._ini()
 
