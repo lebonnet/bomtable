63a24f8bef9c40bba30f9447e0cc8e61374498a7
sag <step.on.off@ya.ru>
2023-02-28 10:50:10 +0300

    eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    root: true,
+    rules: {
+        curly: ['error', 'all'],
+        'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+        'arrow-parens': [0],
+        'prefer-const': 0,
+        'no-irregular-whitespace': 0,
+        'one-var': 0,
+        // 'no-irregular-whitespace': 0,
+        'quote-props': ['error', 'as-needed'],
+        'object-shorthand': [
+            'error',
+            'always',
+            { avoidQuotes: false },
+        ],
+    },
+}
